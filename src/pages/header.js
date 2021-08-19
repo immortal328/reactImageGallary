faf317e10d36299190d292fbaa7863c00cdb3ae4
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-20 01:55:26 +0530

    Update header.js

--- src/pages/header.js
+++ src/pages/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
       return (
             <div className="py-6 text-center bg-green-500">
-                  <h1 className="text-6xl font-extrabold text-green-900">IMAGE GALLARY APP</h1>                  
+                  <h1 className="text-6xl font-extrabold text-green-900">IMAGE GALLERY APP</h1>                  
             </div>
       )
 }
